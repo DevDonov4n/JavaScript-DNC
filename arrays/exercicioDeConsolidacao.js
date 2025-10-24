a0220dd23771978c3699c67baf0b4f56a250506c
Donovan <donovanbueno456@gmail.com>
2025-10-24 13:59:54 -0300

    exercicios de consolidação

---
+++ arrays/exercicioDeConsolidacao.js
@@ -0,0 +1,26 @@
+/*
+criar uma lista de tarefas. cada tarefa sera representada
+por um objeto com as descrição, prioridade e status.
+utilizando os metodos de array e funçoes avançadas vistas no modulo,
+vamos manipular essa lista de tarefas de varias maneiras
+*/
+
+//criar a lista de tarefas
+let tarefas = [
+    {desc: 'academia', prioridade: 1, status: 'pendente' },
+    {desc: 'estudar', prioridade: 2, status: 'concluida' },
+    {desc: 'ler', prioridade: 3, status: 'pendente' },
+    {desc: 'orar', prioridade: 1, status: 'concluida' },
+];
+//adicionar nova tarefa
+tarefas.push({desc: 'limpar a mesa', prioridade: 2, status: 'pendente'});
+
+console.log(tarefas);
+//remove tarefas concluidas
+const tarefasPendentes = tarefas.filter((task) => task.status === 'pendente');
+console.log(tarefasPendentes);
+
+//marcar tarefas concluidas
+
+const tarefasConcluidas = tarefas.filter((task) => task.status === 'concluida');
+console.log(tarefasConcluidas);
